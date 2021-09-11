379b675fa4d62a01430552302330d627769a3d8f
Val Kantchev <vkantchev@users.noreply.github.com>
2021-09-10 23:04:46 -0700

    Auto push on 2021-09-10 23:09

--- samples/windows/enc_avc_file/enc_avc_file.cpp
+++ samples/windows/enc_avc_file/enc_avc_file.cpp
@@ -17,138 +17,140 @@ using namespace std;
 class stdout_utf16
 {
 public:
-	stdout_utf16()
-	{
-		// change stdout to Unicode. Cyrillic and Ideographic characters will appear in the console (console font is unicode).
-		_setmode(_fileno(stdout), _O_U16TEXT);
-	}
+    stdout_utf16()
+    {
+        // change stdout to Unicode. Cyrillic and Ideographic characters will appear in the console (console font is unicode).
+        _setmode(_fileno(stdout), _O_U16TEXT);
+    }
 
-	~stdout_utf16()
-	{
-		// restore ANSI mode
-		_setmode(_fileno(stdout), _O_TEXT); 
-	}
+    ~stdout_utf16()
+    {
+        // restore ANSI mode
+        _setmode(_fileno(stdout), _O_TEXT);
+    }
 };
 
-void printStatus(const primo::error::ErrorInfo* e)
+void printStatus(const primo::error::ErrorInfo *e)
 {
-	if (primo::error::ErrorFacility::Success == e->facility())
-	{
-		wcout << L"Success";
-	}
-	else
-	{
-		wcout << L"facility: " << e->facility() << L", error: " << e->code();
+    if (primo::error::ErrorFacility::Success == e->facility())
+    {
+        wcout << L"Success";
+    }
+    else
+    {
+        wcout << L"facility: " << e->facility() << L", error: " << e->code();
 
-		if (e->message())
-		{
-			wcout << L", " << e->message();
-		}
+        if (e->message())
+        {
+            wcout << L", " << e->message();
+        }
 
-		if (e->hint())
-		{
-			wcout << L", " << e->hint();
-		}
-	}
+        if (e->hint())
+        {
+            wcout << L", " << e->hint();
+        }
+    }
 
-	wcout << endl;
+    wcout << endl;
 }
 
-primo::ref<MediaSocket> createInputSocket(Options& opt)
+primo::ref<MediaSocket> createInputSocket(Options &opt)
 {
-	auto socket = primo::make_ref(Library::createMediaSocket());
-	socket->setStreamType(StreamType::UncompressedVideo);
-	socket->setFile(opt.yuv_file.c_str());
-	
-	auto pin = primo::make_ref(Library::createMediaPin());
-	socket->pins()->add(pin.get());
+    auto socket = primo::make_ref(Library::createMediaSocket());
+    socket->setStreamType(StreamType::UncompressedVideo);
+    socket->setFile(opt.yuv_file.c_str());
 
-	auto vsi = primo::make_ref(Library::createVideoStreamInfo());
-	pin->setStreamInfo(vsi.get());
+    auto pin = primo::make_ref(Library::createMediaPin());
+    socket->pins()->add(pin.get());
 
-	vsi->setStreamType(StreamType::UncompressedVideo);
-	vsi->setFrameWidth(opt.frame_size.width_);
-	vsi->setFrameHeight(opt.frame_size.height_);
-	vsi->setColorFormat(opt.yuv_color.Id);
-	vsi->setFrameRate(opt.fps);
-	vsi->setScanType(ScanType::Progressive);
+    auto vsi = primo::make_ref(Library::createVideoStreamInfo());
+    pin->setStreamInfo(vsi.get());
 
-	return socket;
+    vsi->setStreamType(StreamType::UncompressedVideo);
+    vsi->setFrameWidth(opt.frame_size.width_);
+    vsi->setFrameHeight(opt.frame_size.height_);
+    vsi->setColorFormat(opt.yuv_color.Id);
+    vsi->setFrameRate(opt.fps);
+    vsi->setScanType(ScanType::Progressive);
+
+    return socket;
 }
 
-primo::ref<MediaSocket> createOutputSocket(Options& opt)
+primo::ref<MediaSocket> createOutputSocket(Options &opt)
 {
-	auto socket = primo::make_ref(Library::createMediaSocket());
-	socket->setFile(opt.h264_file.c_str());
-	socket->setStreamType(StreamType::H264);
-	socket->setStreamSubType(StreamSubType::AVC_Annex_B);
+    auto socket = primo::make_ref(Library::createMediaSocket());
+    socket->setFile(opt.h264_file.c_str());
+    socket->setStreamType(StreamType::H264);
+    socket->setStreamSubType(StreamSubType::AVC_Annex_B);
 
-	auto pin = primo::make_ref(Library::createMediaPin());
-	socket->pins()->add(pin.get());
+    auto pin = primo::make_ref(Library::createMediaPin());
+    socket->pins()->add(pin.get());
 
-	auto vsi = primo::make_ref(Library::createVideoStreamInfo());
-	pin->setStreamInfo(vsi.get());
+    auto vsi = primo::make_ref(Library::createVideoStreamInfo());
+    pin->setStreamInfo(vsi.get());
 
-	vsi->setStreamType(StreamType::H264);
-	vsi->setStreamSubType(StreamSubType::AVC_Annex_B);
+    vsi->setStreamType(StreamType::H264);
+    vsi->setStreamSubType(StreamSubType::AVC_Annex_B);
 
-	return socket;
+    return socket;
 }
 
-bool encode(Options& opt)
+bool encode(Options &opt)
 {
-	auto inSocket = createInputSocket(opt);
+    auto inSocket = createInputSocket(opt);
 
-	// create output socket
-	auto outSocket = createOutputSocket(opt);
+    // create output socket
+    auto outSocket = createOutputSocket(opt);
 
-	// create transcoder
-	auto transcoder = primo::make_ref(Library::createTranscoder());
-	transcoder->setAllowDemoMode(TRUE);
-	transcoder->inputs()->add(inSocket.get());
-	transcoder->outputs()->add(outSocket.get());
+    // create transcoder
+    auto transcoder = primo::make_ref(Library::createTranscoder());
+    transcoder->setAllowDemoMode(TRUE);
+    transcoder->inputs()->add(inSocket.get());
+    transcoder->outputs()->add(outSocket.get());
 
-	// transcoder will fail if output exists (by design)
-	deleteFile(opt.h264_file.c_str());
+    // transcoder will fail if output exists (by design)
+    deleteFile(opt.h264_file.c_str());
 
-	wcout << L"Transcoder open: ";
-	if(transcoder->open())
-	{
-		printStatus(transcoder->error());
-		if(!transcoder->run())
-			return false;
+    wcout << L"Transcoder open: ";
+    if (transcoder->open())
+    {
+        printStatus(transcoder->error());
+        if (!transcoder->run())
+            return false;
 
-		wcout << L"Transcoder run: ";
-		printStatus(transcoder->error());
+        wcout << L"Transcoder run: ";
+        printStatus(transcoder->error());
 
-		transcoder->close();
-		wcout << L"Transcoder close: ";
-		printStatus(transcoder->error());
-	}
-	else
-	{
-		printStatus(transcoder->error());
-		return false;
-	}
+        transcoder->close();
+        wcout << L"Transcoder close: ";
+        printStatus(transcoder->error());
+    }
+    else
+    {
+        printStatus(transcoder->error());
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
-int wmain(int argc, wchar_t* argv[])
+int wmain(int argc, wchar_t *argv[])
 {
-	Options opt;
+    Options opt;
 
-	switch(prepareOptions( opt, argc, argv))
-	{
-		case Command: return 0;
-		case Error: return 1;
-	}
+    switch (prepareOptions(opt, argc, argv))
+    {
+    case Command:
+        return 0;
+    case Error:
+        return 1;
+    }
 
-	Library::initialize();
+    Library::initialize();
 
-	bool encodeResult = encode(opt);
+    bool encodeResult = encode(opt);
 
-	Library::shutdown();
+    Library::shutdown();
 
-	return encodeResult ? 0 : 1;
+    return encodeResult ? 0 : 1;
 }
--- samples/windows/enc_avc_file/options.cpp
+++ samples/windows/enc_avc_file/options.cpp
@@ -18,170 +18,163 @@ using namespace primo::program_options;
 using namespace primo::codecs;
 
 ColorDescriptor color_formats[] = {
-	
-	{ ColorFormat::YV12,	L"yv12",		L"Planar Y, V, U (4:2:0) (note V,U order!)" },
-	{ ColorFormat::NV12,	L"nv12",		L"Planar Y, merged U->V (4:2:0)" },
-	{ ColorFormat::YUY2,	L"yuy2",		L"Composite Y->U->Y->V (4:2:2)" },
-	{ ColorFormat::UYVY,	L"uyvy",		L"Composite U->Y->V->Y (4:2:2)" },
-	{ ColorFormat::YUV411,	L"yuv411",		L"Planar Y, U, V (4:1:1)" },
-	{ ColorFormat::YUV420,	L"yuv420",		L"Planar Y, U, V (4:2:0)" },
-	{ ColorFormat::YUV422,	L"yuv422",		L"Planar Y, U, V (4:2:2)" },
-	{ ColorFormat::YUV444,	L"yuv444",		L"Planar Y, U, V (4:4:4)" },
-	{ ColorFormat::Y411,	L"y411",		L"Composite Y, U, V (4:1:1)" },
-	{ ColorFormat::Y41P,	L"y41p",		L"Composite Y, U, V (4:1:1)" },
-	{ ColorFormat::BGR32,	L"bgr32",		L"Composite B->G->R" },
-	{ ColorFormat::BGRA32,	L"bgra32",		L"Composite B->G->R->A" },
-	{ ColorFormat::BGR24,	L"bgr24",		L"Composite B->G->R" },
-	{ ColorFormat::BGR565,	L"bgr565",		L"Composite B->G->R, 5 bit per B & R, 6 bit per G" },
-	{ ColorFormat::BGR555,	L"bgr555",		L"Composite B->G->R->A, 5 bit per component, 1 bit per A" },
-	{ ColorFormat::BGR444,	L"bgr444",		L"Composite B->G->R->A, 4 bit per component" },
-	{ ColorFormat::GRAY,	L"gray",		L"Luminance component only" },
-	{ ColorFormat::YUV420A,	L"yuv420a",		L"Planar Y, U, V, Alpha (4:2:0)" },
-	{ ColorFormat::YUV422A,	L"yuv422a",		L"Planar Y, U, V, Alpha (4:2:2)" },
-	{ ColorFormat::YUV444A,	L"yuv444a",		L"Planar Y, U, V, Alpha (4:4:4)" },
-	{ ColorFormat::YVU9,	L"yvu9",		L"Planar Y, V, U, 9 bits per sample" },
+
+    {ColorFormat::YV12, L"yv12", L"Planar Y, V, U (4:2:0) (note V,U order!)"},
+    {ColorFormat::NV12, L"nv12", L"Planar Y, merged U->V (4:2:0)"},
+    {ColorFormat::YUY2, L"yuy2", L"Composite Y->U->Y->V (4:2:2)"},
+    {ColorFormat::UYVY, L"uyvy", L"Composite U->Y->V->Y (4:2:2)"},
+    {ColorFormat::YUV411, L"yuv411", L"Planar Y, U, V (4:1:1)"},
+    {ColorFormat::YUV420, L"yuv420", L"Planar Y, U, V (4:2:0)"},
+    {ColorFormat::YUV422, L"yuv422", L"Planar Y, U, V (4:2:2)"},
+    {ColorFormat::YUV444, L"yuv444", L"Planar Y, U, V (4:4:4)"},
+    {ColorFormat::Y411, L"y411", L"Composite Y, U, V (4:1:1)"},
+    {ColorFormat::Y41P, L"y41p", L"Composite Y, U, V (4:1:1)"},
+    {ColorFormat::BGR32, L"bgr32", L"Composite B->G->R"},
+    {ColorFormat::BGRA32, L"bgra32", L"Composite B->G->R->A"},
+    {ColorFormat::BGR24, L"bgr24", L"Composite B->G->R"},
+    {ColorFormat::BGR565, L"bgr565", L"Composite B->G->R, 5 bit per B & R, 6 bit per G"},
+    {ColorFormat::BGR555, L"bgr555", L"Composite B->G->R->A, 5 bit per component, 1 bit per A"},
+    {ColorFormat::BGR444, L"bgr444", L"Composite B->G->R->A, 4 bit per component"},
+    {ColorFormat::GRAY, L"gray", L"Luminance component only"},
+    {ColorFormat::YUV420A, L"yuv420a", L"Planar Y, U, V, Alpha (4:2:0)"},
+    {ColorFormat::YUV422A, L"yuv422a", L"Planar Y, U, V, Alpha (4:2:2)"},
+    {ColorFormat::YUV444A, L"yuv444a", L"Planar Y, U, V, Alpha (4:4:4)"},
+    {ColorFormat::YVU9, L"yvu9", L"Planar Y, V, U, 9 bits per sample"},
 };
 
 const int color_formats_len = sizeof(color_formats) / sizeof(ColorDescriptor);
 
-ColorDescriptor* getColorByName(const wchar_t* colorName)
+ColorDescriptor *getColorByName(const wchar_t *colorName)
 {
-	for (int i=0; i < color_formats_len; ++i)
-	{
-		ColorDescriptor& color = color_formats[i];
-		if (compareNoCase(color.name, colorName))
-			return &color;
-	}
-	return NULL;
+    for (int i = 0; i < color_formats_len; ++i)
+    {
+        ColorDescriptor &color = color_formats[i];
+        if (compareNoCase(color.name, colorName))
+            return &color;
+    }
+    return NULL;
 }
 
-ColorDescriptor* getColorById(primo::codecs::ColorFormat::Enum Id)
+ColorDescriptor *getColorById(primo::codecs::ColorFormat::Enum Id)
 {
-	for (int i=0; i < color_formats_len; ++i)
-	{
-		ColorDescriptor& color = color_formats[i];
-		if (color.Id == Id)
-			return &color;
-	}
-	return NULL;
+    for (int i = 0; i < color_formats_len; ++i)
+    {
+        ColorDescriptor &color = color_formats[i];
+        if (color.Id == Id)
+            return &color;
+    }
+    return NULL;
 }
 
-void setDefaultOptions(Options& opt)
+void setDefaultOptions(Options &opt)
 {
-	opt.yuv_file = getExeDir() + L"/../../assets/vid/foreman_qcif.yuv";
-	opt.h264_file = getExeDir() + L"/../../output/enc_avc_file/foreman_qcif.h264";
-	
-	fs::path output(getExeDir() + L"/../../output/enc_avc_file");
-    fs::create_directories(output);    
+    opt.yuv_file = getExeDir() + L"/../../assets/vid/foreman_qcif.yuv";
+    opt.h264_file = getExeDir() + L"/../../output/enc_avc_file/foreman_qcif.h264";
 
-    wostringstream s; 
+    fs::path output(getExeDir() + L"/../../output/enc_avc_file");
+    fs::create_directories(output);
+
+    wostringstream s;
     s << output.c_str() << L"/foreman_qcif.h264";
     opt.h264_file = s.str();
-	
-	opt.frame_size.width_ = 176;
-	opt.frame_size.height_ = 144;
-	opt.fps = 30.0;
-	opt.yuv_color = *getColorById(ColorFormat::YUV420);
+
+    opt.frame_size.width_ = 176;
+    opt.frame_size.height_ = 144;
+    opt.fps = 30.0;
+    opt.yuv_color = *getColorById(ColorFormat::YUV420);
 }
 
-void help(OptionsConfig<wchar_t>& optcfg)
+void help(OptionsConfig<wchar_t> &optcfg)
 {
-	wcout << L"enc_avc_file --frame <width>x<height> --rate <fps> --color <COLOR> --input <file.yuv> --output <file.h264>" << endl
-			<< L"[--colors]" << endl;
-	doHelp(wcout, optcfg);
+    wcout << L"enc_avc_file --frame <width>x<height> --rate <fps> --color <COLOR> --input <file.yuv> --output <file.h264>" << endl
+          << L"[--colors]" << endl;
+    doHelp(wcout, optcfg);
 }
 
-bool validateOptions(Options& opt)
+bool validateOptions(Options &opt)
 {
-	if(opt.yuv_file.empty())
-	{
-		wcout << L"input file needed" << endl;
-		return false;
-	}
-	else
-	{
-		wcout << L"input file: " << opt.yuv_file << endl;
-	}
+    if (opt.yuv_file.empty())
+    {
+        wcout << L"input file needed" << endl;
+        return false;
+    }
+    else
+    {
+        wcout << L"input file: " << opt.yuv_file << endl;
+    }
 
-	if(opt.h264_file.empty())
-	{
-		wcout << L"output file needed" << endl;
-		return false;
-	}
-	else
-	{
-		wcout << L"output file: " << opt.h264_file << endl;
-	}
+    if (opt.h264_file.empty())
+    {
+        wcout << L"output file needed" << endl;
+        return false;
+    }
+    else
+    {
+        wcout << L"output file: " << opt.h264_file << endl;
+    }
 
-	if(opt.fps <= 0.0)
-	{
-		wcout << L"Invalid frame rate" << endl;
-		return false;
-	}
-	else
-	{
-		wcout << L"frame rate: " << opt.fps << endl;
-	}
+    if (opt.fps <= 0.0)
+    {
+        wcout << L"Invalid frame rate" << endl;
+        return false;
+    }
+    else
+    {
+        wcout << L"frame rate: " << opt.fps << endl;
+    }
 
-	if(opt.frame_size.width_ <= 0 || opt.frame_size.height_ <= 0)
-	{
-		wcout << L"Invalid frame size/s" << endl;
-		return false;
-	}
-	else
-	{
-		wcout << L"frame size: " << opt.frame_size.width_ << L"x" << opt.frame_size.height_ << endl;
-	}
+    if (opt.frame_size.width_ <= 0 || opt.frame_size.height_ <= 0)
+    {
+        wcout << L"Invalid frame size/s" << endl;
+        return false;
+    }
+    else
+    {
+        wcout << L"frame size: " << opt.frame_size.width_ << L"x" << opt.frame_size.height_ << endl;
+    }
 
-	if(opt.yuv_color.Id == ColorFormat::Unknown)
-	{
-		wcout << L"Missing or incorrect color format" << endl;
-		return false;
-	}
-	else
-	{
-		wcout << L"color format: " << opt.yuv_color.name << endl;
-	}
+    if (opt.yuv_color.Id == ColorFormat::Unknown)
+    {
+        wcout << L"Missing or incorrect color format" << endl;
+        return false;
+    }
+    else
+    {
+        wcout << L"color format: " << opt.yuv_color.name << endl;
+    }
 
-	return true;
+    return true;
 }
 
 void listColors()
 {
-	wcout << L"\nCOLORS:" << endl;
-	wcout << L"--------------------------------------" << endl;
-	for (int i=0; i < color_formats_len; ++i)
-	{
-		const ColorDescriptor& color = color_formats[i];
-		wcout << left << setw(20) <<  color.name << color.description << endl;
-	}
+    wcout << L"\nCOLORS:" << endl;
+    wcout << L"--------------------------------------" << endl;
+    for (int i = 0; i < color_formats_len; ++i)
+    {
+        const ColorDescriptor &color = color_formats[i];
+        wcout << left << setw(20) << color.name << color.description << endl;
+    }
 }
 
-ErrorCodes prepareOptions(Options &opt, int argc, wchar_t* argv[])
+ErrorCodes prepareOptions(Options &opt, int argc, wchar_t *argv[])
 {
-	if (argc < 2)
+    if (argc < 2)
     {
         setDefaultOptions(opt);
         wcout << L"Using defaults:\n";
         wcout << L"enc_avc_file --input " << opt.yuv_file;
-		wcout << L" --output " << opt.h264_file;
-		wcout << L" --rate " << opt.fps;
-		wcout << L" --frame " << opt.frame_size.width_ << L"x" << opt.frame_size.height_;
-		wcout << L" --color " << opt.yuv_color.name;
+        wcout << L" --output " << opt.h264_file;
+        wcout << L" --rate " << opt.fps;
+        wcout << L" --frame " << opt.frame_size.width_ << L"x" << opt.frame_size.height_;
+        wcout << L" --color " << opt.yuv_color.name;
         wcout << endl;
         return Parsed;
     }
-    
+
     primo::program_options::OptionsConfig<wchar_t> optcfg;
-    optcfg.addOptions()
-            (L"help,h",			opt.help,								L"")
-            (L"input,i",		opt.yuv_file, wstring(),				L"input YUV file")
-			(L"output,o",		opt.h264_file, wstring(),				L"output H264 file")
-			(L"rate,r",			opt.fps, 0.0,							L"input frame rate")
-			(L"frame,f",		opt.frame_size, FrameSize(),			L"input frame sizes <width>x<height>")
-			(L"color,c",		opt.yuv_color, ColorDescriptor(),		L"input color format. Use --colors to list all supported color formats")
-			(L"colors",			opt.list_colors,						L"list COLOR constants");
-       
+    optcfg.addOptions()(L"help,h", opt.help, L"")(L"input,i", opt.yuv_file, wstring(), L"input YUV file")(L"output,o", opt.h264_file, wstring(), L"output H264 file")(L"rate,r", opt.fps, 0.0, L"input frame rate")(L"frame,f", opt.frame_size, FrameSize(), L"input frame sizes <width>x<height>")(L"color,c", opt.yuv_color, ColorDescriptor(), L"input color format. Use --colors to list all supported color formats")(L"colors", opt.list_colors, L"list COLOR constants");
+
     try
     {
         primo::program_options::scanArgv(optcfg, argc, argv);
@@ -192,18 +185,18 @@ ErrorCodes prepareOptions(Options &opt, int argc, wchar_t* argv[])
         help(optcfg);
         return Error;
     }
-    
+
     if (opt.help)
     {
         help(optcfg);
         return Command;
     }
 
-	if(opt.list_colors)
-	{
-		listColors();
-		return Command;
-	}
+    if (opt.list_colors)
+    {
+        listColors();
+        return Command;
+    }
 
     if (!validateOptions(opt))
     {
@@ -211,32 +204,31 @@ ErrorCodes prepareOptions(Options &opt, int argc, wchar_t* argv[])
         return Error;
     }
 
-	return Parsed;
+    return Parsed;
 }
 
 std::wistringstream &operator>>(std::wistringstream &in, FrameSize &frameSize)
 {
-	in >> frameSize.width_;
+    in >> frameSize.width_;
 
-	wchar_t ch;
-	
-	in >> ch;
-	
-	in >> frameSize.height_;
-	return in;
+    wchar_t ch;
+
+    in >> ch;
+
+    in >> frameSize.height_;
+    return in;
 }
 
 std::wistringstream &operator>>(std::wistringstream &in, ColorDescriptor &colorDesc)
 {
-	std::wstring strColorName;
-	in >> strColorName;
+    std::wstring strColorName;
+    in >> strColorName;
 
-	ColorDescriptor* pc = getColorByName(strColorName.c_str());
-	if(!pc)
-		throw ParseFailure<wchar_t>(L"", strColorName, L"Parse error");
+    ColorDescriptor *pc = getColorByName(strColorName.c_str());
+    if (!pc)
+        throw ParseFailure<wchar_t>(L"", strColorName, L"Parse error");
 
-	colorDesc = *pc;
+    colorDesc = *pc;
 
-	return in;
+    return in;
 }
-
