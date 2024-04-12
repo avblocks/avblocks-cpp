c3b3292c546ac21092bc0ffae8d24a35d6f45741
vkantchev <vkantchev@users.noreply.github.com>
2024-04-12 13:49:37 -0700

    Auto push on 2024-04-12 13:49:37

--- samples/windows/re-encode/util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-inline std::wstring getExeDir()
-{
-    WCHAR exedir[MAX_PATH];
-    GetModuleFileName(NULL, exedir, MAX_PATH);
-    PathRemoveFileSpec(exedir);
-    return std::wstring(exedir);
-}
