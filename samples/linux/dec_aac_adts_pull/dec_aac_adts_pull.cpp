b2c554103055ce9b71586c08dfc2b006a3b40ff0
vkantchev <vkantchev@users.noreply.github.com>
2025-11-15 22:41:00 -0800

    Auto push on 2025-11-15 22:41:00

---
+++ samples/linux/dec_aac_adts_pull/dec_aac_adts_pull.cpp
@@ -0,0 +1,164 @@
+#include <primo/avblocks/avb.h>
+#include <primo/platform/reference++.h>
+#include <primo/platform/ustring.h>
+
+#include "util.h"
+#include "options.h"
+
+using namespace primo::codecs;
+using namespace primo::avblocks;
+using namespace std;
+
+primo::ref<MediaSocket> createDecoderOutputSocket()
+{
+    auto socket = primo::make_ref(Library::createMediaSocket());
+    socket->setStreamType(StreamType::LPCM);
+
+    auto pin = primo::make_ref(Library::createMediaPin());
+    auto asi = primo::make_ref(Library::createAudioStreamInfo());
+    asi->setStreamType(StreamType::LPCM);
+    asi->setChannels(2);
+    asi->setSampleRate(48000);
+    asi->setBitsPerSample(16);
+
+    pin->setStreamInfo(asi.get());
+    socket->pins()->add(pin.get());
+
+    return socket;
+}
+
+primo::ref<Transcoder> createWavWriter(const std::string &outputFile)
+{
+    // input stream info, pin and socket
+    auto infmt = primo::make_ref(Library::createAudioStreamInfo());
+    infmt->setStreamType(StreamType::LPCM);
+    infmt->setChannels(2);
+    infmt->setSampleRate(48000);
+    infmt->setBitsPerSample(16);
+
+    auto inPin = primo::make_ref(Library::createMediaPin());
+    inPin->setStreamInfo(infmt.get());
+
+    auto inSocket = primo::make_ref(Library::createMediaSocket());
+    inSocket->setStreamType(StreamType::LPCM);
+    inSocket->pins()->add(inPin.get());
+
+    // output stream info, pin and socket
+    auto outfmt = primo::make_ref(infmt->clone());
+    
+    auto outPin = primo::make_ref(Library::createMediaPin());
+    outPin->setStreamInfo(outfmt.get());
+
+    auto outSocket = primo::make_ref(Library::createMediaSocket());
+    outSocket->setStreamType(StreamType::WAVE);
+    outSocket->pins()->add(outPin.get());
+
+    // set output file
+    outSocket->setFile(primo::ustring(outputFile));
+
+    // create transcoder
+    auto wavWriter = primo::make_ref(Library::createTranscoder());
+    wavWriter->setAllowDemoMode(true);
+    wavWriter->inputs()->add(inSocket.get());
+    wavWriter->outputs()->add(outSocket.get());
+
+    return wavWriter;
+}
+
+bool decode(Options &opt)
+{
+    // transcoder will fail if output exists (by design)
+    deleteFile(primo::ustring(opt.outputFile));
+
+    // Create decoder transcoder
+    auto decoder = primo::make_ref(Library::createTranscoder());
+    decoder->setAllowDemoMode(true);
+
+    auto inputSocket = primo::make_ref(Library::createMediaSocket());
+    inputSocket->setFile(primo::ustring(opt.inputFile));
+    decoder->inputs()->add(inputSocket.get());
+    decoder->outputs()->add(createDecoderOutputSocket().get());
+
+    if (!decoder->open())
+    {
+        printError("Decoder open", decoder->error());
+        return false;
+    }
+
+    // Create WAV writer transcoder
+    auto wavWriter = createWavWriter(opt.outputFile);
+    if (!wavWriter->open())
+    {
+        printError("WAV Writer open", wavWriter->error());
+        decoder->close();
+        return false;
+    }
+
+    // Pull-push decoding loop
+    int32_t decoderOutputIndex = 0;
+    auto pcmSample = primo::make_ref(Library::createMediaSample());
+
+    bool decoderEos = false;
+    while (!decoderEos)
+    {
+        // Pull PCM sample from decoder
+        if (decoder->pull(decoderOutputIndex, pcmSample.get()))
+        {
+            // Push PCM sample to WAV writer
+            if (!wavWriter->push(0, pcmSample.get()))
+            {
+                printError("WAV Writer push", wavWriter->error());
+                decoder->close();
+                wavWriter->close();
+                return false;
+            }
+
+            continue;
+        }
+
+        // No more PCM data from decoder
+        const primo::error::ErrorInfo *error = decoder->error();
+        if (error->facility() == primo::error::ErrorFacility::Codec &&
+            error->code() == primo::codecs::CodecError::EOS)
+        {
+            // Push null to signal EOS to WAV writer
+            wavWriter->push(0, nullptr);
+            decoderEos = true;
+
+            continue;
+        }
+
+        printError("Decoder pull", error);
+        decoder->close();
+        wavWriter->close();
+        return false;
+    }
+
+    decoder->close();
+    wavWriter->close();
+
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    Options opt;
+
+    switch (prepareOptions(opt, argc, argv))
+    {
+    case Command:
+        return 0;
+    case Error:
+        return 1;
+    case Parsed:
+        break;
+    }
+
+    Library::initialize();
+
+    bool result = decode(opt);
+
+    Library::shutdown();
+
+    return result ? 0 : 1;
+}
